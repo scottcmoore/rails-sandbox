84ac4e78156033af833666540ccdd7826dc210cd
Scott Moore <scott@trelora.com>
2020-05-17 11:40:56 -0600

    Replace comment bodies (#2)
    Add a callback to replace comment bodies before they are committed to
    the database. Blog comments are always spam anyways, right?

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,14 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  before_save :equivalent_value_comment
+
+  # Never read the comments.
+  def equivalent_value_comment
+    new_body = ""
+    (self.body.length / 5).times do
+      new_body << (rand(2) > 0 ? "herp " : "derp ")
+    end
+    self.body = new_body.rstrip
+  end
+
 end
