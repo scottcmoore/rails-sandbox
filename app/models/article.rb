8fc0c54255b3ab3ecc08918679eacb6c5931728a
Scott Moore <scott@trelora.com>
2020-05-18 09:13:15 -0600

    Allow comments to be discarded
    Update comments to allow them to be discarded/soft deleted. Only show
    kept/undiscarded comments on articles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
   include Discard::Model
+  # Since we only soft delete articles, Article#destroy never gets called.
+  # As a result cleaning up comments on discarded articles is handled by
+  # the Comment kept scope, per the discard gem docs. As a result, no
+  # dependent: :destroy is used here.
   has_many :comments
 
   validates :title, presence: true, length: { minimum: 5 }
